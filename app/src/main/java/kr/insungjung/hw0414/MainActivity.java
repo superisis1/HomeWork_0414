90091ce68c934a715fe694a2d7d04089edcd3d06
Insung Jung <superisis@gmail.com>
2019-04-16 01:46:37 +0900

    MainActivity.java 파일에 xml의 컴포넌트에 해당하는 변수 선언

--- app/src/main/java/kr/insungjung/hw0414/MainActivity.java
+++ app/src/main/java/kr/insungjung/hw0414/MainActivity.java
@@ -2,6 +2,9 @@ package kr.insungjung.hw0414;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText mUserName = findViewById(R.id.nameText);
+        EditText mUserEmail = findViewById(R.id.emailText);
+        CheckBox mCheckAll = findViewById(R.id.allCheck);
+        CheckBox mCheck1 = findViewById(R.id.check1);
+        CheckBox mCheck2 = findViewById(R.id.check2);
+        CheckBox mCheck3 = findViewById(R.id.check3);
+        CheckBox mCheck4 = findViewById(R.id.check4);
+        Button mButton = findViewById(R.id.nextBtn);
+
+
+
+
     }
 }
